7be151b3e7e1c5dc5bdc0e5cc10f5da841bc1d0f
Matthew C.O <matconol@epsg.upv.es>
2021-01-21 17:55:01 +0100

    Detected left hand only

--- cam.py
+++ cam.py
@@ -9,6 +9,7 @@ Author: Matthew Conde Oltra
 import cv2
 import mediapipe as mp
 
+
 class VideoStream(object):
     
     #Initialize important variables
@@ -19,7 +20,7 @@ class VideoStream(object):
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_hands = mp.solutions.hands
         # For webcam input - initialize MediaPipe Hands
-        self.hands = self.mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.hands = self.mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
     #
     def __del__(self):
 
@@ -29,6 +30,7 @@ class VideoStream(object):
 
         # Read each frame 
         ret, frame = self.video.read()
+        
         # Flip the image horizontally for a later selfie-view display, and convert
         # the BGR image to RGB.
         image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB) 
@@ -36,17 +38,58 @@ class VideoStream(object):
         # pass by reference.
         image.flags.writeable = False
         results = self.hands.process(image)
-
         # Draw hand landmarks of each hand
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-          for hand_landmarks in results.multi_hand_landmarks:
-            # Draw index finger tip coordinates
-            self.mp_drawing.draw_landmarks(
-                image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+        image_height, image_width, _ = image.shape
+        #Sí no encuentra manos no hará nada, pero devolverá image
+        if results.multi_handedness == None:
+            #no me pintará nada
+            print('Type value is None')
+            # Convert to jpeg
+            ret, jpeg = cv2.imencode('.jpg', image)
 
-        # Convert to jpeg
-        ret, jpeg = cv2.imencode('.jpg', image)
+            return jpeg.tobytes()
+        #Sí encuentra manos identificara cual es
+        else:
+            #print('Handedness: ', results.multi_handedness[0].classification[0].label)
+            what_hand = results.multi_handedness[0].classification[0].label
+            #Si la mano es la izquierda dibujara las landmarks
+            if what_hand == 'Left':
+                print('Left hand')
+                
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
 
-        return jpeg.tobytes()
+                        """print(
+                            f'Thumb finger tip coordinates: (',
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_TIP].x * image_width}, '
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_TIP].y * image_height})'
+                            f'\nIndex finger tip coordinates: (',
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+                            f'\nMiddle finger tip coordinates: (',
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * image_width}, '
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image_height})'
+                            f'\nRing finger tip coordinates: (',
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_TIP].x * image_width}, '
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_TIP].y * image_height})'
+                            f'\nPinky finger tip coordinates: (',
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_TIP].x * image_width}, '
+                            f'{hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_TIP].y * image_height})'
+                        )"""
+                        
+                        # Draw index finger tip coordinates
+                        self.mp_drawing.draw_landmarks(
+                            image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+                # Convert to jpeg
+                ret, jpeg = cv2.imencode('.jpg', image)
+
+                return jpeg.tobytes()
+            #sí la mano es la derecha no dibujara landmarks pero si devolvera image
+            else:
+                print('Debes utilizar la mano derecha')
+                # Convert to jpeg
+                ret, jpeg = cv2.imencode('.jpg', image)
+
+                return jpeg.tobytes()
